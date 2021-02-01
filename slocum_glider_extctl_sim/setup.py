912f1da49d56f7b051d3c8bfb9ddcd1d698bc562
Eric Timmons <etimmons@mit.edu>
2021-02-01 16:43:31 -0500

    Rename slocum_glider_sim_driver -> slocum_glider_extctl_sim

--- slocum_glider_extctl_sim/setup.py
+++ slocum_glider_extctl_sim/setup.py
@@ -5,8 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['slocum_glider_sim_driver',
-              'slocum_glider_sim_driver.behaviors'],
+    packages=['slocum_glider_extctl_sim',
+              'slocum_glider_extctl_sim.behaviors'],
     package_dir={'': 'src'},
 )
 
