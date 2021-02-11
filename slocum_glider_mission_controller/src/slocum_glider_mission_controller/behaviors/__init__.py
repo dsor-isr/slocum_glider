c2af82d153cf9a7a87bcacc5fb6a38e89a96ff8d
Eric Timmons <etimmons@mit.edu>
2021-02-11 10:35:29 -0500

    mission_controller: Add thruster_constant_power action

--- slocum_glider_mission_controller/src/slocum_glider_mission_controller/behaviors/__init__.py
+++ slocum_glider_mission_controller/src/slocum_glider_mission_controller/behaviors/__init__.py
@@ -3,13 +3,15 @@ from .go_to_waypoint import GoToWaypointBehavior
 from .go_to_waypoint_list import GoToWaypointListBehavior
 from .stay_at_waypoint import StayAtWaypointBehavior
 from .surface import SurfaceBehavior
+from .thruster_constant_power import ThrusterConstantPowerBehavior
 from .thruster_off import ThrusterOffBehavior
 from .yo import YoBehavior
 
 
 BEHAVIOR_CLASSES = [FollowBottomBehavior, GoToWaypointBehavior,
                     GoToWaypointListBehavior, StayAtWaypointBehavior,
-                    SurfaceBehavior, ThrusterOffBehavior, YoBehavior]
+                    SurfaceBehavior, ThrusterConstantPowerBehavior,
+                    ThrusterOffBehavior, YoBehavior]
 
 BEHAVIOR_CLASS_MAP = {cls.ACTION_NAME: cls for cls in BEHAVIOR_CLASSES}
 
