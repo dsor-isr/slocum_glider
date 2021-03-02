be2bc046cbe52721029d724c676e69654b22fd17
Eric Timmons <etimmons@mit.edu>
2021-03-02 11:37:16 -0500

    Add event handling code to mission

--- slocum_glider_mission_controller/src/slocum_glider_mission_controller/missions/mission.py
+++ slocum_glider_mission_controller/src/slocum_glider_mission_controller/missions/mission.py
@@ -15,10 +15,13 @@ added or removed depending on the policies of the concrete implementations.
     REQUIRED_CONTROLS = set(['heading', 'pitch', 'bpump', 'thruster'])
     MAX_UNSAFE_DURATION = 8
 
-    def __init__(self, behaviors):
+    def __init__(self, behaviors, event_handlers=[]):
         self.start_time = rospy.get_time()
         self.last_safe_time = rospy.get_time()
         self.behaviors = behaviors
+        self.event_handlers = event_handlers
+        self.active_event_handler = None
+        self.paused_behaviors = []
 
     def start(self, g):
         """Start the mission. g is a reference to the glider's extctl interface. Simply
@@ -41,6 +44,69 @@ into the STOPPED state.
                 to_remove.append(b)
         for b in to_remove:
             self.behaviors.remove(b)
+
+        # If we're handling an event and become unsafe, that means we've
+        # finished handling the event.
+        if self.active_event_handler and not self.is_actively_controlled():
+            self.stop_handling_event(g)
+
+    def fire_event(self, g, event):
+        """Determine if any event handler wants to respond to the event. If so, begin
+processing it.
+
+        """
+        # We currently don't allow nesting event handlers. Once we start
+        # responding to one we need to finish it before responding to another.
+        if self.handling_event:
+            return
+
+        for h in self.event_handlers:
+            if h.should_fire(g, event):
+                self.begin_handling_event(g, h)
+                return
+
+    def begin_handling_event(self, g, handler):
+        """Register handler as the active event handler. Start all of its behaviors and
+add them to the list of active behaviors, pausing any existing behavior that
+controls the same things.
+
+        """
+        self.active_event_handler = handler
+        behaviors = handler.behaviors
+        paused_behaviors = []
+
+        # Ensure each behavior is initialized.
+        for b in behaviors:
+            b.state = 'READY'
+            b.start(g)
+            # Pause any existing behavior that conflicts.
+            for existing_behavior in self.behaviors.copy():
+                if b.CONTROLS.intersection(existing_behavior.CONTROLS):
+                    paused_behaviors.pause(g)
+                    paused_behaviors.append(existing_behavior)
+                    self.behaviors.remove(existing_behavior)
+
+        self.behaviors.extend(behaviors)
+        self.paused_behaviors = paused_behaviors
+
+    def stop_handling_event(self, g):
+        """Remove the active event handler. Remove all its behaviors from the active
+list and resume all paused behaviors.
+
+        """
+        handler = self.active_event_handler
+        behaviors = handler.behaviors
+        paused_behaviors = self.paused_behaviors
+
+        for b in behaviors:
+            self.behaviors.remove(b)
+
+        for b in paused_behaviors:
+            self.behaviors.append(b)
+            b.resume(g)
+
+        self.active_event_handler = None
+        self.paused_behaviors = []
 
     def end(self, g):
         for b in self.behaviors:
@@ -67,14 +133,21 @@ successful.
         """
         pass
 
-    def is_safe(self):
-        """Returns True iff the mission is currently in a safe state."""
+    def is_actively_controlled(self):
+        """Returns True if all required controls are being controlled by some active
+behavior.
+
+        """
         actively_controlled = set()
         for b in self.behaviors:
             actively_controlled.update(b.CONTROLS)
+        return actively_controlled == self.REQUIRED_CONTROLS
+
+    def is_safe(self):
+        """Returns True iff the mission is currently in a safe state."""
 
         current_time = rospy.get_time()
-        if actively_controlled == self.REQUIRED_CONTROLS:
+        if self.is_actively_controlled():
             self.last_safe_time = current_time
             return True
         else:
