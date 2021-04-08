3c507169524346a5a785d2f0e323b5773e63a29a
Eric Timmons <etimmons@mit.edu>
2021-04-08 12:10:53 -0400

    Forgot to disable a mode

--- slocum_glider_mission_controller/src/slocum_glider_mission_controller/behaviors/surface.py
+++ slocum_glider_mission_controller/src/slocum_glider_mission_controller/behaviors/surface.py
@@ -64,6 +64,7 @@ Takes 2 parameters.
                 # We need to exit the surface mode otherwise we'll never stop
                 # trying to surface!
                 self.disable_mode(MODE_NORMAL_SURFACE_BIT)
+                self.disable_mode(MODE_NORMAL_SURFACE_RESUME_BIT)
                 self.substate = 'WAITING_FOR_BEHAVIOR_TO_END'
         elif self.substate == 'WAITING_FOR_BEHAVIOR_TO_END':
             if not g.state.x_surface_active:
