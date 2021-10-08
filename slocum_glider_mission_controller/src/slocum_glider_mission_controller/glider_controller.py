11e457319c08d349787f78451c5999ff647fb645
Eric Timmons <etimmons@mit.edu>
2021-10-08 16:07:10 -0400

    Sleep more

--- slocum_glider_mission_controller/src/slocum_glider_mission_controller/glider_controller.py
+++ slocum_glider_mission_controller/src/slocum_glider_mission_controller/glider_controller.py
@@ -106,6 +106,7 @@ Future iterations will likely also sprial in place and set the thruster to max.
                           and g.state.u_mission_param_l == 2):
                         # user has asked for a status update!
                         if gave_status:
+                            rate.sleep()
                             continue
                         gave_status = True
                         g.send_file(
