104d04b8b5b1b489fd04c67b3729c77d19400d36
Eric Timmons <etimmons@mit.edu>
2022-03-17 02:53:37 -0400

    More logging, fix infinite loop preempting an action

--- slocum_glider_mission_controller/src/slocum_glider_mission_controller/action_server.py
+++ slocum_glider_mission_controller/src/slocum_glider_mission_controller/action_server.py
@@ -1,4 +1,5 @@
 import actionlib
+import rospy
 
 
 class BehaviorActionServer(object):
@@ -11,6 +12,7 @@ class BehaviorActionServer(object):
         self.action_class = action_class
 
         self.active_goal = None
+        self.preempting = False
 
         self.server = actionlib.SimpleActionServer(name,
                                                    action_class,
@@ -26,6 +28,9 @@ class BehaviorActionServer(object):
         if goal is None:
             return
 
+        rospy.loginfo("Got new goal for behavior %s:\n%s",
+                      self.behavior_class,
+                      goal)
         if self.server.is_preempt_requested():
             self.server.set_preempted(
                 text="Preempted before starting execution"
@@ -39,24 +44,39 @@ class BehaviorActionServer(object):
         if result:
             self.active_behavior = behavior
         else:
+            rospy.logwarn(
+                'Unable to activate behavior %s. Has the mission started?',
+                self.behavior_class
+            )
             self.server.set_aborted(
                 text="Unable to activate behavior"
             )
 
     def preempt_cb(self):
+        rospy.loginfo('Goal preempted for %s:\n', self.behavior_class)
+        self.preempting = True
         # This could technically return False, but the only way that happens
         # currently is in static missions (where goals will never be accepted)
         # or dynamic missions (in which case this behavior as already ended).
         self.glider_controller.stop_behavior(self.active_behavior)
+        rospy.loginfo('Finished preempting for %s:\n', self.behavior_class)
+        self.preempting = False
 
     def send_feedback(self, feedback):
         self.server.publish_feedback(feedback)
 
     def abort(self, result=None, text=''):
+        if self.preempting:
+            return
+        rospy.loginfo('Aborting goal for %s:\n%s', self.behavior_class, text)
         self.server.set_aborted(result=result, text=text)
 
     def preempt(self, result=None, text=""):
+        if self.preempting:
+            return
+        rospy.loginfo('Preempting goal for %s:\n%s', self.behavior_class, text)
         self.server.set_preempted(result=result, text=text)
 
     def succeed(self, result=None, text=""):
+        rospy.loginfo('succeeding goal for %s:\n%s', self.behavior_class, text)
         self.server.set_succeeded(result=result, text=text)
